adbe4cee4e86ddb4723e0185ef6256853b6fdbea
anamdiazs <anamdiazs1212@gmail.com>
2022-05-23 15:51:06 -0500

    config: Configure and Install Firebase on Android

---
+++ classtime-app/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDZ6G1Mejw4R9hPwSPQ9twoHIkX-pHJvQ',
+  authDomain: 'classtime-349918.firebaseapp.com',
+  databaseURL: '',
+  projectId: 'classtime-349918',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
+initializeApp(firebaseConfig);
