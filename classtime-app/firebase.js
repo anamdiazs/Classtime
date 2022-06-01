858958078c17f74b7d913541e4e4d878f5998f8f
Ana María Díaz <anamdiazs1212@gmail.com>
2022-06-01 00:51:07 -0500

    config: Finally firebase working correctly

--- classtime-app/firebase.js
+++ classtime-app/firebase.js
@@ -1,17 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: 'AIzaSyDDZ6G1Mejw4R9hPwSPQ9twoHIkX-pHJvQ',
-  authDomain: 'classtime-349918.firebaseapp.com',
-  databaseURL: '',
-  projectId: 'classtime-349918',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: '1:874724046701:android:322d3877a8410f0099cc64',
-  measurementId: 'G-measurement-id',
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDZ6G1Mejw4R9hPwSPQ9twoHIkX-pHJvQ",
+  authDomain: "classtime-349918.firebaseapp.com",
+  projectId: "classtime-349918",
+  storageBucket: "classtime-349918.appspot.com",
+  messagingSenderId: "874724046701",
+  appId: "1:874724046701:web:466854f4e8ac533599cc64",
+  measurementId: "G-8W4QNMBYFF"
 };
 
-initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
