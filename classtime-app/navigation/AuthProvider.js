4f4ca67eabdf3f13c690a475cf0895a90252d9b8
Ana María Díaz <anamdiazs1212@gmail.com>
2022-06-02 02:39:54 -0500

    fix: Comment Auth Provider

--- classtime-app/navigation/AuthProvider.js
+++ classtime-app/navigation/AuthProvider.js
@@ -10,23 +10,13 @@
 
 // export const AuthProvider = ({ children }) => {
 	
-// 	const [accessToken, setAccessToken] = useState();
-// 	const [userInfo, setUserInfo] = useState();
-// 	const [message, setMessage] = useState();
-	
 // 	return (
 // 		<UserContext.Provider
 // 		value = {{
 // 			userInfo,
 // 			setUserInfo, 
 // 			googleLogin: async () => {
-// 				console.log('Inside google login')
 // 				try{
-// 					const [request, response, promptAsync] = Google.useAuthRequest({
-// 					iosClientId: "874724046701-3jgh4bfnknk0qfrbik4mtkof4c2b6slj.apps.googleusercontent.com",
-// 					expoClientId: "694235095257-7t7h7mv877d2jfu7r508ct1egmesbqdm.apps.googleusercontent.com"
-// 					});
-
 
 // 					} catch(err){
 // 						console.log('Inside catch block' )
